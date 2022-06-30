f68d5c49aa20662b7ad96f9de7da933f7d84d8e4
Bradley Wilson <bradleywilson727@gmail.com>
2022-06-30 10:56:13 -0500

    made alterations

--- SQLJoinsExercise.sql
+++ SQLJoinsExercise.sql
@@ -6,7 +6,7 @@ SELECT * FROM sales;
 
 /* joins: select all the computers from the products table:
 using the products table and the categories table, return the product name and the category name */
-SELECT products.name, categories.name 
+SELECT products.name AS 'Product Name', categories.name AS 'Category Name'
 FROM products JOIN categories ON products.CategoryID = categories.CategoryID WHERE products.CategoryID = 1;
 
 /* joins: find all product names, product prices, and products ratings that have a rating of 5 */
@@ -14,18 +14,22 @@ SELECT products.name, products.price, reviews.rating
 FROM products JOIN reviews ON products.productid = reviews.ProductID WHERE reviews.rating = 5;
  
 /* joins: find the employee with the most total quantity sold.  use the sum() function and group by */
-SELECT SUM(sales.quantity), employees.firstname, employees.LastName 
+SELECT SUM(sales.quantity) AS 'Total Sold', employees.firstname AS 'First Name', employees.LastName AS 'Last Name' 
 FROM employees JOIN sales ON employees.EmployeeID = sales.EmployeeID 
-GROUP BY employees.EmployeeID;
+GROUP BY employees.FirstName, employees.LastName
+ORDER BY 'total sold' DESC;-- this 'order by' isn't working correctly and I'm not sure why.
 
 /* joins: find the name of the department, and the name of the category for Appliances and Games */
-SELECT departments.name, categories.name FROM departments JOIN categories 
-ON departments.DepartmentID = categories.DepartmentID WHERE categories.name = 'appliances' OR categories.name = 'games';
+SELECT departments.name, categories.name 
+FROM departments JOIN categories 
+ON departments.DepartmentID = categories.DepartmentID 
+WHERE categories.name = 'appliances' OR categories.name = 'games';
 
 /* joins: find the product name, total # sold, and total price sold,
  for Eagles: Hotel California --You may need to use SUM() */
- SELECT products.name, SUM(sales.Quantity), sales.PricePerUnit*sales.quantity AS total_price
- FROM products JOIN sales ON products.ProductID = sales.ProductID WHERE products.ProductID = 97 GROUP BY total_price;
+ SELECT products.name, SUM(sales.Quantity) AS 'Total Sold', (products.price*SUM(sales.Quantity)) AS 'Total Price Sold'
+ FROM products JOIN sales ON products.ProductID = sales.ProductID 
+ WHERE products.ProductID = 97;
 
 /* joins: find Product name, reviewer name, rating, and comment on the Visio TV. (only return for the lowest rating!) */
 SELECT products.name, reviews.Reviewer, reviews.rating, reviews.comment FROM products 
